e44b483eaa7723ba694f860e7cd294c2415689a6
Acla Putra <muhamadacla22@gmail.com>
2021-12-06 04:37:44 +0700

    modified blogitem

--- client/src/components/molecules/BlogItem/index.js
+++ client/src/components/molecules/BlogItem/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { RegisterBg } from '../../../assets'
 import { useHistory } from 'react-router-dom'
 import './blogItem.scss'
-import { Button } from '../../atoms'
+import { Button, Gap } from '../../atoms'
 
 const BlogItem = () => {
     const history = useHistory()
@@ -13,6 +13,7 @@ const BlogItem = () => {
                 <p className='title'>Title Blog</p>
                 <p className='author'>Author - Date post</p>
                 <p className='body'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it Ato make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <Gap height={20} />
                 <Button title="View Detail" onClick={() => history.push('/detail-blog')} />
             </div>
         </div>
